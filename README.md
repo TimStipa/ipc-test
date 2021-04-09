8a89c79d3bc1fe575702494aab1d1a3b50003415
Tim Stipa <timothy.a.stipa@gmail.com>
2021-04-09 12:05:51 -0400

    Added python test client. Refactored repository structure.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This is a test repo for verifying the behavior of IPC mechanisms (both on linux hosts and in a k8s cluster).
 
 It includes source code for example producer/consumers of Unix domain sockets, as well as dockerfiles for producing container images.
+
+Example clients are controlled in their respective directories:
+* c
+* python
---
+++ python/ipc-test.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+import socket
+import sys
+import time
+
+# Address for Unix Domain Socket
+SOCKET_ADDRESS = './ipc_test'
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "--num_messages",
+            type=int, 
+            help="The number of messages to send. Only used when running as the producer")
+    parser.add_argument(
+            "--producer",
+            help="Run as the IPC producer. Users must specify only one of the \'consumer\' or \'producer\' flags.",
+            action="store_true")
+    parser.add_argument(
+            "--consumer",
+            help="Run as the IPC consumer. Users must specify only one of the \'consumer\' or \'producer\' flags.",
+            action="store_true")
+
+    return parser.parse_args()
+
+def consumer():
+    print("Consumer: Running as consumer")
+
+    # Connect to the socket established by the producer and wait for data
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(SOCKET_ADDRESS)
+    try:
+        print("Consumer: Connected to Producer")
+        flag = True
+        while flag:
+            data = sock.recv(1024).decode()
+            print("Consumer: Received message = {}".format(data))
+            if data == 'Exit':
+                flag = False;
+            time.sleep(1)
+    finally:
+        print("Consumer: Closing socket")
+        sock.close()
+        
+
+def unbindSocket():
+    try:
+        os.unlink(SOCKET_ADDRESS)
+    except OSError:
+        if os.path.exists(SOCKET_ADDRESS):
+            raise
+
+def producer(messages):
+    print("Producer: Running as producer")
+    if messages is None:
+        print("Producer: Num messages was not set. Aborting.");
+        exit(1)
+   
+    # Ensure socket is not already in use
+    unbindSocket()
+
+    # Establish new socket and wait for connection.
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.bind(SOCKET_ADDRESS)
+    sock.listen(1)
+
+    completed = True;
+    while completed:
+        print("Producer: Waiting for client...")
+        connection, client = sock.accept()
+
+        # Once a connection is established, send the specified number of messages.
+        # These messages will be sent at 5 second intervals.
+        try:
+            print("Producer: Found client")
+            for x in range(messages):
+                tmp_message = 'Sending Message: {}'.format(x)
+                connection.send(tmp_message.encode('utf-8'))
+                time.sleep(5)
+            connection.send('Exit'.encode('utf-8'))
+            completed = False
+        finally:
+            print("Producer: Closing connection")
+            connection.close()
+
+
+
+def main(args):
+    if args.consumer and args.producer:
+        print("Invalid arguments. Cannot be both producer and consumer.")
+        exit(1)
+    if args.consumer:
+        consumer()
+    elif args.producer:
+        producer(args.num_messages)
+    else:
+        print("No argument specified")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main(parseArgs());
